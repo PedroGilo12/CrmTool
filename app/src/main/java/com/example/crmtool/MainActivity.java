be3fd4804423d45e1c4e44e06c02b191cf929480
Pedro Giló <pedro.gilo@edge.ufal.br>
2023-11-27 21:55:17 -0300

    Cria tela inicial.

--- app/src/main/java/com/example/crmtool/MainActivity.java
+++ app/src/main/java/com/example/crmtool/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.crmtool;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button managerUserButton = (Button) findViewById(R.id.managerUserButton);
+
+        managerUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.println(Log.INFO, "managerUserButtonLog", "Manager Button Pressed");
+            }
+        });
+
     }
 }
